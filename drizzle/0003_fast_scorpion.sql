cd918a9717019c55a74c4a257009632597df717c
okparaa <iamokpara@gmail.com>
2024-12-03 18:17:01 +0100

    remove drizzle

--- drizzle/0003_fast_scorpion.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "students" ADD COLUMN "description" varchar;
--- drizzle/0004_warm_imperial_guard.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "description" varchar;--> statement-breakpoint
-ALTER TABLE "students" DROP COLUMN IF EXISTS "description";
